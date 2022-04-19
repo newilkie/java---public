cd80e92fe753bac4eaa05802340e5745770cc4f6
newilkie <62954091+newilkie@users.noreply.github.com>
2022-04-18 22:50:53 -0400

    Composition, Encapsulation Lectures
    Adding the composition, encapsulation lectures coding challenges and code.

---
+++ ChallengeHouseConstructor/src/com/company/Floor.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Floor {
+    private String floorType;
+    private String floorColor;
+
+    public Floor(String floorType, String floorColor) {
+        this.floorType = floorType;
+        this.floorColor = floorColor;
+    }
+
+    public String getFloorType() {
+        return floorType;
+    }
+
+    public String getFloorColor() {
+        return floorColor;
+    }
+}
