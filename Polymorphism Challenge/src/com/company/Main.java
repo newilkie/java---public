5af3181678a20bca54bda32ea114ad44c25d4060
newilkie <62954091+newilkie@users.noreply.github.com>
2022-04-20 21:56:22 -0400

    Polymorphism Lecture and Challenge
    Ran through and learned how to use some of the navigation in intellij ideas. Ran through and did the polymorphism lecture lecture's 95 and 96.

---
+++ Polymorphism Challenge/src/com/company/Main.java
@@ -0,0 +1,154 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// We are going to go back to the car analogy.
+    // Create a base class called Car
+    // It should have a few fields that would be appropriate for a generic car class.
+        // engine, cylinders, wheels, etc.
+        //Constructor should initialize cylinders (number of) and name, and set the wheels to 4
+        // and engine to true. cylinders and names would be passed parameters.
+        //
+        // Create appropriate getters
+        //
+        // Create some methods like startEngine, accelerate, and brake
+        //
+        // show a message for each in the base class
+        // Now create 3 subclasses for your favorite vehicles.
+        // Override the appropriate methods to demonstrate polymorphism in use.
+        // put all classes in the one java file (this one).
+
+        Car car = new Car("Lancer", 6);
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi("Lancer", 6);
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Ford ford = new Ford("F150", 6);
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Holden holden = new Holden("Holden Commodore ", 6);
+        System.out.println(holden.startEngine());
+        System.out.println(holden.accelerate());
+        System.out.println(holden.brake());
+
+
+    }
+}
+
+class Car{
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(String name,  int cylinders) {
+        this.engine = true;
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+    }
+
+    public String startEngine(){
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate(){
+        return "Car -> accelerate()";
+    }
+
+    public String brake(){
+        return "Car -> brake()";
+    }
+
+    public boolean isEngine() {
+        return engine;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Mitsubishi extends Car{
+    public Mitsubishi(String name, int cylinders) {
+        super(name, cylinders);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
+}
+
+class Ford extends Car{
+    public Ford(String name, int cylinders) {
+        super(name, cylinders);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Ford -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Ford -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Ford -> brake()";
+    }
+}
+
+class Holden extends Car{
+    public Holden(String name, int cylinders) {
+        super(name, cylinders);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() + " -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " -> brake()";
+    }
+}
+
+
+
+
+
